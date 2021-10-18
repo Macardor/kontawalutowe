a912de96d2b2c1aa84ddc3fcc607038028d4b928
Macardor <macardorpl@gmail.com>
2021-10-18 15:46:44 +0200

    Main imports clean up

--- src/main/java/com/kaczmarz/kontawalutowe/KontawalutoweApplication.java
+++ src/main/java/com/kaczmarz/kontawalutowe/KontawalutoweApplication.java
@@ -1,15 +1,13 @@
 package com.kaczmarz.kontawalutowe;
 
-import org.json.simple.parser.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
 
 @SpringBootApplication
 public class KontawalutoweApplication {
 
-	public static void main(String[] args) throws IOException, ParseException {
+	public static void main(String[] args) {
 
 		SpringApplication.run(KontawalutoweApplication.class, args);
 	}
